85ad69d4aabd17537026da719d196bbc57a66935
zeeshana <zeeshanahmed1b@gmail.com>
2019-10-26 23:23:09 +0300

    initt

---
+++ app.js
@@ -0,0 +1,44 @@
+//require("@babel/register");
+//require("babel-polyfill");
+const result = require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+//const logger = require('express-logger');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const cors = require('cors');
+const sessions = require('./controllers/sessionsController');
+const app = express();
+
+if (result.error) {
+  throw result.error
+}
+ 
+console.log(result.parsed)
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//app.use(logger({ path: "log/express.log" }));
+app.use(cookieParser());
+console.log(process.env.SESSION_SECRET);
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+/*
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    path: '/',
+    httpOnly: true
+  }
+  }));
+  */
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+app.use('/sessions', sessions);
+app.listen(8080, () => {
+  console.log('App running on port 8080!');
+});
